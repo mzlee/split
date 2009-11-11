bc76855d0735902bf8728f4a42d718fdf20a046a
Jim Brusstar <jim.brusstar@gmail.com>
2009-11-11 17:08:55 -0500

    Revert "Cleaned up master branch"
    This reverts commit cd14bc36dc6a6eb85273f60f64f5ddab1a519eb5.

---
+++ ben_experimental/WebWindow.h
@@ -0,0 +1,75 @@
+#ifndef WEBWINDOW_H
+#define WEBWINDOW_H
+#include "ui_WebWindow.h"
+#include <QString>
+#include <QWidget>
+#include <QFocusEvent>
+#include <QStateMachine>
+#include <QByteArray>
+
+class WebWindow : public QDialog {
+
+	Q_OBJECT //this needs to be here for signals/slots
+
+public:
+
+	WebWindow( QWidget *parent = 0 );
+	
+	//navigate the WebWindow's page to url
+	void navigate( QString url );
+
+        void startClipping();
+
+        void exitClipping();
+
+        //this removes the basic mask
+        void removeMask();
+
+
+signals:
+	
+	//this is emitted when a window gains focus
+	void wwFocus(WebWindow *ww);
+
+        //this is emitted when a window is restored
+        void wwRestoreWindow(WebWindow *ww);
+
+protected:
+	
+	//this is a virtual event function that must be implemented
+	//(used to keep track of window focus)
+	void changeEvent( QEvent *event );
+
+        //this is a virtual event function that updates the size of the webview and the mouse tracking
+        void resizeEvent(QResizeEvent *);
+
+        //this sets up the state machine on the web window
+        void setupState();
+
+private slots:
+
+        //this creates the basic mask
+        void createMask(QRegion region);
+
+        //stores coordinates of mask
+        void storeMask(QRegion region);
+private:
+
+        Ui::WebWindow ui;
+
+        QPalette overlay_palette;
+
+        bool focusCounter;
+
+        // qt State Machine
+        QStateMachine machine;
+
+        // window geometry
+        QByteArray window_geometry;
+
+        //Where the mask is
+        QRegion mask_region;
+        bool mask_region_set;
+};
+
+#endif //WEBWINDOW_H
